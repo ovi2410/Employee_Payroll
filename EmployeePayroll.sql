382b4fe8af3349663106f5620cbccdaa110ad0d3
ovi2410 <guneshwarifegade@gmail.com>
2022-06-14 15:37:28 +0530

    [ovi]insert value

--- EmployeePayroll.sql
+++ EmployeePayroll.sql
@@ -9,3 +9,12 @@ name varchar(200) not null,
 salary float,
 startDate date
 )
+
+--UC-3 Insert values in Table
+Insert into empoyee_payrolls(name,salary,startDate) values
+('shalini',20000,'2021-03-12'),
+('Magesh',25000,'2021-04-18'),
+('Gayathri',10000,'2020-05-13'),
+('Aruna',30000,'2020-08-19');
+select * from empoyee_payrolls;
+
